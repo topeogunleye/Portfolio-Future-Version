b24ad75faac1fc0ee77b2a1929bc0a4643f9ae60
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-11-08 20:09:25 +0100

    Add type attribute to button, Reduce transition time on button to 0.3s

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
 // https://codepen.io/ljc-dev/embed/GRoLWxj?height=600&default-tab=js,result&embed-version=2
 // https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
-var navbar = document.querySelector('.navbar'); // list
-var ham = document.querySelector('.ham'); //button
+const navbar = document.querySelector('.navbar'); // list
+const ham = document.querySelector('.ham'); // button
+const menuLinks = document.querySelectorAll('.menuLink');
 
-// toggles hamburger menu in and out when clicking on the hamburger
-function toggleHamburger() {
-  navbar.classList.toggle('showNav');
-  ham.classList.toggle('showClose');
-}
-
-ham.addEventListener('click', toggleHamburger);
-
-// toggle when clicking on links
-var menuLinks = document.querySelectorAll('.menuLink');
 menuLinks.forEach((menuLink) => {
+  // toggles hamburger menu in and out when clicking on the hamburger
+  function toggleHamburger() {
+    navbar.classList.toggle('showNav');
+    ham.classList.toggle('showClose');
+  }
+
+  ham.addEventListener('click', toggleHamburger);
+
+  // toggle when clicking on links
+
   menuLink.addEventListener('click', toggleHamburger);
 });
 
@@ -24,11 +25,12 @@ const sections = document.querySelectorAll('section', 'footer');
 const navLi = document.querySelectorAll('.navbar ul li a');
 
 window.onscroll = () => {
-  var current = '';
+  let current = '';
 
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
 
+    // eslint-disable-next-line no-restricted-globals
     if (scrollY >= sectionTop - 60) {
       current = section.getAttribute('id');
       // console.log(current);
@@ -40,7 +42,6 @@ window.onscroll = () => {
     li.classList.remove('active');
 
     if (li.classList.contains(current)) {
-      console.log(li)
       li.classList.add('active');
     }
   });
